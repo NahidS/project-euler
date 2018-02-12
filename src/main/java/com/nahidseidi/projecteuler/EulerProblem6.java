480002abb55899b9fda515903f2373ca1007a1f4
NahidS <nahid.seidi@gmail.com>
2018-02-12 12:53:52 +0100

    Added problem 6

---
+++ src/main/java/com/nahidseidi/projecteuler/EulerProblem6.java
@@ -0,0 +1,14 @@
+package com.nahidseidi.projecteuler;
+
+public class EulerProblem6 implements IEulerProblem {
+
+   public long solve() {
+      long squareOfSum = 0, sumOfSquare = 0;
+      for (int number = 1; number <= 100; number++) {
+         squareOfSum += number;
+         sumOfSquare += (number * number);
+      }
+      return (squareOfSum * squareOfSum) - sumOfSquare;
+   }
+
+}
